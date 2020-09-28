2b84182fffdadd5e08ac3070212aa0651d764ddd
bdb2381 <brad.beggs@gmail.com>
2020-09-27 22:15:45 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -4,15 +4,31 @@ export function manageFriends(state, action){
       return {
         ...state, friends: [...state.friends, action.friend] 
       }
-    case "REMOVE_FRIEND":
-      const removalIndexOfFriend = state.friends.findIndex(friend => friend.id === action.id)
-      return (
-        {...state, 
-        friends: [
-          ...state.friends.slice(0, removalIndexOfFriend),
-        ...state.friends.slice(removalIndexOfFriend + 1)]
-      }
-      )
+   // solution method and my method below
+      // case "REMOVE_FRIEND":
+    //   const removalIndexOfFriend = state.friends.findIndex(friend => friend.id === action.id)
+    //   return (
+    //     {...state, 
+    //     friends: [
+    //       ...state.friends.slice(0, removalIndexOfFriend),
+    //     ...state.friends.slice(removalIndexOfFriend + 1)]
+    //   }
+    //   )
+      case "REMOVE_FRIEND":
+        const allKeptFriends = state.friends.filter(friend => friend.id != action.id)
+
+        return ({
+          ...state, friends: allKeptFriends
+        })
+
+
+
+
+
+
+
+
+
     default: 
       return state
   }
